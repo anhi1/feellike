545d3fa473a0ace601581b92bc7eca847ce995b5
Mani Shidfar <mreza.shidfar@gmail.com>
2023-06-08 20:11:34 +0200

    casa gallery component updated

--- src/app/casas/casa-gallery/casa-gallery.component.ts
+++ src/app/casas/casa-gallery/casa-gallery.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { ICasa } from '../models/casa.model';
+import { CasaService } from './../services/casa.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-casa-gallery',
   templateUrl: './casa-gallery.component.html',
   styleUrls: ['./casa-gallery.component.css']
 })
-export class CasaGalleryComponent {
+export class CasaGalleryComponent implements OnInit{
 
+  casas: ICasa[] = [];
+
+constructor(private casaService: CasaService){}
+
+ngOnInit(): void {
+
+  this.casaService.findAll().subscribe(data => {
+    console.log(data);
+    this.casas = data
+  } )
+}
 }
