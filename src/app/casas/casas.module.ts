20e8db8ce7749b0d82933af4429f0dd54e375a76
Mani Shidfar <mreza.shidfar@gmail.com>
2023-06-08 20:32:28 +0200

    httpClientModule imported in casa.module

--- src/app/casas/casas.module.ts
+++ src/app/casas/casas.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HttpClientModule } from '@angular/common/http';
 import { CasasRoutingModule } from './casas-routing.module';
 import { CasaListComponent } from './casa-list/casa-list.component';
 import { CasaGalleryComponent } from './casa-gallery/casa-gallery.component';
@@ -17,7 +17,8 @@ import { CasaFormComponent } from './casa-form/casa-form.component';
   ],
   imports: [
     CommonModule,
-    CasasRoutingModule
+    CasasRoutingModule,
+    HttpClientModule
   ]
 })
 export class CasasModule { }
