4ce544ffcacdbdd469b59fdc23e0f58badfea8dd
Mani Shidfar <mreza.shidfar@gmail.com>
2023-06-08 21:37:09 +0200

    casa detalle in proccess

--- src/app/casas/casa-detail/casa-detail.component.ts
+++ src/app/casas/casa-detail/casa-detail.component.ts
@@ -1,4 +1,7 @@
+import { CasaService } from './../services/casa.service';
 import { Component } from '@angular/core';
+import { ICasa } from '../models/casa.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-casa-detail',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./casa-detail.component.css']
 })
 export class CasaDetailComponent {
+casa?: ICasa;
+constructor(private casaService: CasaService, private activateroute: ActivatedRoute){}
+ngOnInit(): void {
+  this.activateroute.params.subscribe(params => {
+    const id = parseInt(params['id'], 10);
+    this.casaService.findById(id).subscribe(data => this.casa = data)
+})
+}
 
+reserve(){}
 }
