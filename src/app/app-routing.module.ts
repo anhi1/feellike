221ff38cc09150b16a3f3acba4135b2ba2b2922a
DaysiDelgado <daydelgadod@gmail.com>
2023-06-08 13:51:27 +0200

    routing casas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CasaGalleryComponent } from './casas/casa-gallery/casa-gallery.component';
+import { CasaFormComponent } from './casas/casa-form/casa-form.component';
+import { CasaListComponent } from './casas/casa-list/casa-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'casa-gallery', loadChildren:() => import('./casas/casas.module').then(m=>m.CasasModule)},
+  {path: 'casa-list', component: CasaListComponent},
+  {path: 'casa-form', component: CasaFormComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/casas/casas-routing.module.ts
+++ src/app/casas/casas-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CasaGalleryComponent } from './casa-gallery/casa-gallery.component';
+import { CasaDetailComponent } from './casa-detail/casa-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CasaGalleryComponent},
+  {path: ':id', component: CasaDetailComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
