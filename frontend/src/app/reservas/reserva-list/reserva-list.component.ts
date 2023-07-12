284cfa13a35c4a933a87dac246eeb8299b1e1971
anhi1 <anhiemolina@gmail.com>
2023-07-12 20:28:46 +0000

    reserv casa

--- frontend/src/app/reservas/reserva-list/reserva-list.component.ts
+++ frontend/src/app/reservas/reserva-list/reserva-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IReserva } from '../reserva.model';
+import { ReservaService } from '../services/reserva.service';
 
 @Component({
   selector: 'app-reserva-list',
   templateUrl: './reserva-list.component.html',
   styleUrls: ['./reserva-list.component.css']
 })
-export class ReservaListComponent {
+export class ReservaListComponent implements OnInit {
+
+  displayedColumns: string[] = [
+    'price',
+    'startDate',
+    'finishDate',
+    'actions',
+  ];
+  reservas: IReserva[] = [];
+
+  constructor(private reservaService: ReservaService) {}
+
+  ngOnInit(): void {
+    this.reservaService.findAll()
+    .subscribe(data => this.reservas = data);
+  }
+
 
 }
