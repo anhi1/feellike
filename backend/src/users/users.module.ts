82418068d3936110636031fd0d3d64669e235607
DaysiDelgado <daydelgadod@gmail.com>
2023-07-18 18:24:11 +0200

    subida de imagen backend

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,13 +1,32 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 import { User } from './users.model';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import { extname } from 'path';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService],
+  imports: [
+    MulterModule.register({
+      storage: diskStorage({
+        // carpeta destino donde se guardarán los archivos interceptados en los controladores
+        destination: './uploads',
+        // definir cómo se genera el nombre del archivo antes de guardarlo en la carpeta uploads
+        filename: (req, file, callback) => {
+          let fileName = uuidv4() + extname(file.originalname);
+          callback(null, fileName);
+        }
+      })
+    }),
+    TypeOrmModule.forFeature([User])
+  ],
   controllers: [UsersController],
-  exports:[UsersService]
+  providers: [UsersService],
+  exports: [UsersService]
 })
 export class UsersModule {}
+
+
